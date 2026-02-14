3561c2ee27d1d9f40e3de5fa3d5b7208465f76c8
Aiko Pras <a.pras@utwente.nl>
2026-02-14 13:57:08 +0100

    Update DCCHardware.cpp
    The #ifdef structure to include the correct file is extended for ESP32-RMT, SAMD and the checks for traditional Arduino's is improved.

--- src/DCCHardware.cpp
+++ src/DCCHardware.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 
 #define Z21PG
-#include <Arduino.h>
-
-#define Z21PG
 
 // Create a define for all DxCore variants, to improve readability
 #if defined(__AVR_DA__) || defined(__AVR_DB__) || defined(__AVR_DD__) || \
@@ -11,35 +8,66 @@
   #define AVR_DXCORE
 #endif
 
+// Same for the ATMega Timer1 support
+#if defined(__AVR_ATmega328P__)  || defined(__AVR_ATmega2560__) || \
+    defined(__AVR_ATmega1280__) || defined(__AVR_ATmega32U4__) || \
+    defined(__AVR_ATmega644P__)  || defined(__AVR_ATmega644PA__)
 
+  #define ATMEGA_HAS_TIMER1
+#endif
+
+
+// ================= Z21PG =================
 #if defined(Z21PG)
 
-  // ---------------- AVR DxCore ----------------
+  // AVR DxCore
   #if defined(AVR_DXCORE)
     #include "variants-Z21PG/DCCHardware_dxcore_sw_tcb1.inc"
 
-  // ---------------- ESP32 (all) ----------------
+  // Traditional AVRs
+  #elif defined(ATMEGA_HAS_TIMER1)
+    #include "variants-Z21PG/DCCHardware_atmega_sw_timer1.inc"
+
+  // ESP32 (all)
   #elif defined(ARDUINO_ARCH_ESP32)
 
-    // ---- ESP32-C series (RISC-V) ----
+    //  ESP32-C series (RISC-V)
     #if defined(CONFIG_IDF_TARGET_ESP32C3) || \
         defined(CONFIG_IDF_TARGET_ESP32C5) || \
         defined(CONFIG_IDF_TARGET_ESP32C6)
 
       #include "variants-Z21PG/DCCHardware_ESP32C.inc"
 
-    // ---- ESP32-S series (Xtensa: ESP32 / S2 / S3) ----
+    // ESP32-S series (Xtensa: ESP32 / S2 / S3)
     #else
       #include "variants-Z21PG/DCCHardware_ESP32S.inc"
     #endif
 
-  // ---------------- Other AVR ----------------
+  // SAMD processors
+  #elif defined(ARDUINO_ARCH_SAMD)
+    #include "variants-Z21PG/DCCHardware_SAMD_sw_timer.inc"
+
+  // Others
   #else
-    #include "variants-Z21PG/DCCHardware_atmega_sw_timer1.inc"
+    #error No DCCHardware implementation for this processor / board (Z21PG mode)
   #endif
 
 
-// ================= Non-Z21PG =================
-#elif defined(AVR_DXCORE)
-  #include "variants-HQ/DCCHardware_dxcore_hw_tca0.inc"
+
+// ================= HQ =================
+#else
+
+  // AVR DxCore
+  #if defined(AVR_DXCORE)
+    #include "variants-HQ/DCCHardware_DxCore_TCA0.inc"
+
+  // ESP32 (all)
+  #elif defined(ARDUINO_ARCH_ESP32)
+    #include "variants-HQ/DCCHardware-ESP32-RMT.inc"
+
+  // Others
+  #else
+    #error No DCCHardware implementation for this processor / board (HQ mode)
+  #endif
+
 #endif
